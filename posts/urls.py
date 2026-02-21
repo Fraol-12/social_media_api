cdeda092310d0b8a1ab39b3cb811d7b1de0659c8
Fraol <fraolm41@gmail.com>
2026-02-21 20:37:21 +0300

    refactor(posts): views file the way alx require

--- posts/urls.py
+++ posts/urls.py
@@ -1,12 +1,18 @@
-from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet, LikePostView, UnlikePostView
+from .views import PostViewSet, CommentViewSet, LikePostView, UnlikePostView, FeedView
 from django.urls import path
+from rest_framework_nested import routers 
 
-router = DefaultRouter()
-router.register(r'', PostViewSet, basename='post')
-router.register(r'comments', CommentViewSet, basename='comment')
 
-urlpatterns = router.urls + [
+router = routers.SimpleRouter() 
+router.register(r'', PostViewSet, basename='post')  
+
+posts_router = routers.NestedSimpleRouter(router, r'', lookup='post')
+posts_router.register(r'comments', CommentViewSet, basename='post-comments')
+
+
+urlpatterns = router.urls + posts_router.urls + [
+    path('feed/', FeedView.as_view(), name='feed'),
     path('<int:pk>/like/', LikePostView.as_view(), name='like-post'),
     path('<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
+
